d8dc2ccac7f20461cc5ff31528f6a63ad5370fb4
101552111 <arevolloserrano@gmail.com>
2025-10-08 16:11:11 -0400

    Lab Ex 1 example

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,6 +1,3 @@
-//This project uses the Ratlib
-
-
 #include "raylib.h"
 #include "raymath.h"
 #define RAYGUI_IMPLEMENTATION
@@ -8,40 +5,42 @@
 #include "game.h"
 
 const unsigned int TARGET_FPS = 50;
+float dt = 1.0f / TARGET_FPS;
 float time = 0;
-float dt;
 float x = 500;
 float y = 500;
-float frequency = 1.0f;
-float amplitude = 70;
+float frequency = 1;
+float amplitude = 100;
 
+// Change world state
 void update()
 {
-  time += 1.0 / TARGRT_FPS;
+  time += 1.0 / TARGET_FPS;
   time += dt;
 
-x = x + (-sin)(time * frequency)) * frequency * amplitude * dt;
-y = y + (cos)(time * frequency)) * frequency * amplitude * dt;
+  x = x + (-sin(time * frequency)) * frequency * amplitude * dt;
+  y = y + (cos(time * frequency)) * frequency * amplitude * dt;
 }
 
+// Display world state
 void draw()
 {
-BeginDrawing();
-ClearBackground(RED);
-DrawText("Joss Moo-Young 123456789", 10, 10, 20, LIGHTGRAY);
+ BeginDrawing();
+ ClearBackground(BLACK);
+ DrawText("Alejandro Revollo-Serrano 101552111", 10, float(GetScreenHeight()- 30), 20, LIGHTGRAY);
 
-GuiSliderBar(Rectangle{ 60, GetScreenheight, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
-DrawText(TextFormat ("FPS: %f, TIME: %.2f") ("FPS:", GetScreenWidth() - 400, 20, 20, LIGHTGRAY);
+ GuiSliderBar(Rectangle{ 60, 15, 1000, 20 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
+ DrawText(TextFormat ("T: %6.2f", time), GetScreenWidth() - 140, 10, 30, LIGHTGRAY);
 
-DrawCircle(x, y, 60, RED);
-DrawCircle(GetScreenWidth ()/2 + cos(time * frequency) * amplitude, GetScreenHeight()/2 + min(time * frequency) * amplitude, 60, GREEN);
+ DrawCircle(x, y, 70, RED);
+ DrawCircle(500 + cos(time * frequency) * amplitude, 500 + sin (time * frequency) * amplitude, 70, GREEN);
 
 EndDrawing();
 }
 
 int main()
 {
-    InitWindow(1200, 800, "Physics Labs: Joss Moo-Young 123456789");
+    InitWindow(1200, 800, "GAME2005: Alejandro Revollo-Serrano 101552111");
     SetTargetFPS(TARGET_FPS);
 
     while (!WindowShouldClose())
